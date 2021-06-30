b54c62f22d10a1021c0cabea39677729eb2c8455
Maciej <maciej.glitwinski@gmail.com>
2021-06-30 11:18:55 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,11 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection")); 
             });
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy",policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
         }
 
@@ -56,6 +61,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
