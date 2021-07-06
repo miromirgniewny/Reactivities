8caf59ae6bfce6d37982374b8fa46aa00508e39e
Maciej <maciej.glitwinski@gmail.com>
2021-07-06 13:31:43 +0200

    End of section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,15 @@
-import React, { Fragment, useEffect} from 'react';
+import React, { Fragment} from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 
 export default function App() {
@@ -14,16 +18,22 @@ export default function App() {
 
   return (
     <Fragment>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <Route exact path='/' component={HomePage}  />
       <Route 
         path={'/(.+)'}
         render={() => (
           <>
             <NavBar />
-            <Container style={{marginTop: '7em'}}>
+              <Container style={{ marginTop: '7em' }}>
+              <Switch>
               <Route exact path='/activities' component={ActivityDashboard}  />
               <Route path='/activities/:id' component={ActivityDetails}  />
               <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm}  />
+              <Route path={'/errors'} component={TestErrors} />
+              <Route path={'/server-error'} component={ServerError} />
+              <Route component={NotFound} />
+              </Switch>
             </Container>    
           </>
         )}
